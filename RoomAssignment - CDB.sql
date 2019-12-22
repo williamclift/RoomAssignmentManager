e99e686427a3cea86de35807984e69523ed43afb
William Clift <54684237+williamclift@users.noreply.github.com>
2019-12-21 22:00:04 -0500

    Create RoomAssignment - CDB.sql
    The Create Database file for the Room Assignment Database.
    -- Currently no tables -- 12/21/2019

---
+++ RoomAssignment - CDB.sql
@@ -0,0 +1,14 @@
+-- Create a new database called 'RoomAssignment'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT [name]
+        FROM sys.databases
+        WHERE [name] = N'RoomAssignment'
+)
+CREATE DATABASE RoomAssignment
+GO
+
+CREATE TABLE()
